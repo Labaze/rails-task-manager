c3bea0d7536d569181b557d65ea0eabed966ceb6
Basile Masson <basilemasson2@gmail.com>
2020-02-11 17:15:52 +0100

    Taskss

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_111438) do
+ActiveRecord::Schema.define(version: 2020_02_11_111729) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
